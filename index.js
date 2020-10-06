55d23113e80b73477c483947efb41db49a63c6d7
Rawhatur Rabbi Rafin <rawhaturrafin@gmail.com>
2020-10-06 19:18:33 +0600

    edited

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const MongoClient = require('mongodb').MongoClient;
 require("dotenv").config();
-const MongoClient = require("mongodb").MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i4bni.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k3xzf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+
+
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
 
 const app = express();
 
